7b264c75884d3da62c11a4adb791618845011e98
maxl <lunga@dynamicdevices.ch>
2020-03-24 11:15:28 +0100

    - first implementation of not-working arcgis overwrite script

--- python-scripts/arcgis_push.py
+++ python-scripts/arcgis_push.py
@@ -1,20 +1,42 @@
 # Import libraries
-import pandas as pd
 from arcgis.gis import GIS
 from arcgis import features
 from arcgis.features import FeatureLayerCollection
+import pandas as pd
+import datetime
+
+files = {
+    'dd-covid19-openzh-cantons-latest.csv': '2b7c0d6c1d944acaaaf323f0719804c8',
+    'dd-covid19-openzh-switzerland-latest.csv': 'a48bbcb06c8e4b629a31e5584c5e991a'
+    }
 
 # Connect to the GIS
-gis = GIS("https://ddrobotec.maps.arcgis.com", "cybermax")
+gis = GIS('https://ddrobotec.maps.arcgis.com', 'cybermax', 'am1koNe123!!')
 
-# Load csv from and add the csv as an item
-canton_latest_csv_file = "../data-cantons-csv/dd-covid19-ch-cantons-latest.csv"
-#switzerland_latest_csv_file = "../data-switzerland-csv/dd-covid19-ch-switzerland-latest.csv"
+for f in files:
+    # Load csv from and add the csv as an item
+    latest_csv_file = './output_openzh/' + f
 
-# Add the csv as an item
-canton_latest_item = gis.content.get("c646a9a1727743aaa4162235d798b058")
-#switzerland_latest_item = gis.content("1100451fd22d42c1bc2b18776429a8a4")
+    # Add the csv as an item using der ids
+    latest_csv_item = gis.content.get(files[f])
 
-feature_layer_collection = FeatureLayerCollection.fromitem(canton_latest_item)
-res = feature_layer_collection.manager.overwrite(canton_latest_csv_file)
-print(res)
+    print("-----")
+
+    print("Accessing feature server: " + latest_csv_item.url)
+    print("Found feature layer %s on server" % latest_csv_item.title)
+
+    # latest_csv_item.update(data=latest_csv_file)
+
+    print("Overwriting existing feature with %s ..." % f)
+    # fs = latest_csv_item.publish(overwrite=True)
+
+    # Get feature layer collection from item
+    flc = FeatureLayerCollection.fromitem(latest_csv_item)
+
+    print(type(flc))
+
+    # Overwrite old item with new item
+    print(latest_csv_file)
+    res = flc.manager.overwrite(latest_csv_file)    
+    #print(res)
+
