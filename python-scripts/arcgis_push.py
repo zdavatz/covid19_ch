9875b59250e19576f1f6678fea46f7ed550f3e20
Emilio <32411099+weentermute@users.noreply.github.com>
2020-03-24 13:39:04 +0100

    - cleaned up script a little - added development data

--- python-scripts/arcgis_push.py
+++ python-scripts/arcgis_push.py
@@ -7,19 +7,16 @@ import datetime
 import os
 
 files = {
-    'dd-covid19-openzh-cantons-latest.csv': '71d789eb72e8413abc6590e41a7f3cb2',
-    'dd-covid19-openzh-switzerland-latest.csv': 'a48bbcb06c8e4b629a31e5584c5e991a'
+    'devel-cantons-latest.csv': '0b12cddba60e49aa9ebe07c3f38cde30',
+    # 'dd-covid19-openzh-switzerland-latest.csv': 'a48bbcb06c8e4b629a31e5584c5e991a'
     }
 
 def file_path():
     return os.path.dirname(os.path.abspath(__file__))  + "/output_openzh/"
 
-# Connect to the GIS
-gis = GIS('https://ddrobotec.maps.arcgis.com', 'cybermax', 'hauptstrasse84!!')
-
-for f in files:
+def update_from_csv(gis : GIS, f):
     # Load csv from and add the csv as an item
-    latest_csv_file = file_path() + f
+    latest_csv_file = os.path.join(file_path(), f)
 
     # Add the csv as an item using der ids
     latest_csv_item = gis.content.get(files[f])
@@ -29,7 +26,7 @@ for f in files:
     print("Accessing feature server: " + latest_csv_item.url)
     print("Found feature layer %s on server" % latest_csv_item.title)
 
-    #latest_csv_item.update(data=latest_csv_file)
+    latest_csv_item.update(data=latest_csv_file)
 
     print("Overwriting existing feature with %s ..." % f)
     #fs = latest_csv_item.publish(overwrite=True, file_type='featureService')
@@ -42,5 +39,23 @@ for f in files:
     # Overwrite old item with new item
     # print(latest_csv_file)
     res = flc.manager.overwrite(latest_csv_item)    
-    #print(res)
+    print(res)
 
+def publish_from_csv(gis : GIS, f : str):
+    # Load csv from and add the csv as an item
+    latest_csv_file = os.path.join(file_path(), f[0])
+
+    item_prop = {'title': f.replace("-", "_").replace(".csv", "") }
+    csv_item = gis.content.add(item_properties=item_prop, data=latest_csv_file)
+
+    # publish the csv item into a feature layer
+    published_item = csv_item.publish()
+
+    print(published_item)
+
+
+# Connect to the GIS
+gis = GIS('https://ddrobotec.maps.arcgis.com', 'cybermax', os.environ['ARCGIS_PASS'])
+
+for f in files:
+    update_from_csv(gis, f)
