68931ff980341abf891254cfcce726a09085985a
maxl <lunga@dynamicdevices.ch>
2020-03-27 10:51:48 +0100

    - generated new latest files - removed unnecessary devel- files

--- python-scripts/digest_baryluk.py
+++ python-scripts/digest_baryluk.py
@@ -5,11 +5,12 @@ import pandas as pd
 from common_data import *
 
 def download_baryluk_data():
-    functor_xyz = pd.read_csv("https://www.functor.xyz/covid_19/scrapers/outputs/latest.txt", sep='\s+', engine='python', keep_default_na=True, header=None).fillna()
+    functor_xyz = pd.read_csv("https://www.functor.xyz/covid_19/scrapers/outputs/latest.txt", sep='\s+', engine='python', error_bad_lines=False, keep_default_na=True, header=None)
     return functor_xyz
 
 def process_baryluk_data(table):
     df = pd.DataFrame(table)
+    print(df)
     print(df.shape)
 
 if __name__ == '__main__':
