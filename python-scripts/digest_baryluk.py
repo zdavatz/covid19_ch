af115e4fd26c2f2fc5d7ffd32459bb6c6f328f0a
maxl <lunga@dynamicdevices.ch>
2020-03-27 23:06:42 +0100

    - added parser for scraper data and simple sum-based comparison between scrapers and OpenZH

--- python-scripts/digest_baryluk.py
+++ python-scripts/digest_baryluk.py
@@ -5,7 +5,7 @@ import pandas as pd
 from common_data import *
 
 def download_baryluk_data():
-    functor_xyz = pd.read_csv("https://www.functor.xyz/covid_19/scrapers/outputs/latest.txt", sep='\s+', engine='python', error_bad_lines=False, keep_default_na=True, header=None)
+    functor_xyz = pd.read_csv("http://pillbox.oddb.org/current.txt", sep='\s+', engine='python', error_bad_lines=False, keep_default_na=True, header=None)
     return functor_xyz
 
 def process_baryluk_data(table):
